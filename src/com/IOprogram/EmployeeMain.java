a4dd574dffef2721f620dedb612809644edd5597
kapilmahajan123 <montyempire123gmail.com>
2023-01-05 17:13:08 +0530

    Added: Stored Employees to File

--- src/com/IOprogram/EmployeeMain.java
+++ src/com/IOprogram/EmployeeMain.java
@@ -5,7 +5,10 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 /**
  * 
@@ -13,30 +16,58 @@ import java.util.Scanner;
  *
  */
 public class EmployeeMain {
-	
 	/**
+	 * Create an employee payroll service to store Employee payroll into a file
+	 */
+	private static int empId;
+    private static String empName;
+    private static double empSalary;
+    private static Scanner scanner;
+    
+    private static void inputEmpPayroll() {
+        System.out.print("Enter Employee Id: ");
+        empId = scanner.nextInt();
+        System.out.print("Enter Employee Name: ");
+        empName = scanner.next();
+        System.out.print("Enter Employee Salary: ");
+        empSalary = scanner.nextDouble();
+
+    }
+    /**
 	 * Create and to read and write employee payroll to a console
-	 * @param args
 	 */
     public static void main(String[] args) {
         System.out.println("Welcome to File IO!");
 
-        Scanner scanner = new Scanner(System.in);
+        String option;
+        scanner = new Scanner(System.in);
 
-        System.out.print("Enter Employee Id: ");
-        int empId = scanner.nextInt();
-        System.out.print("Enter Employee Name: ");
-        String empName = scanner.next();
-        System.out.print("Enter Employee Salary: ");
-        double empSalary = scanner.nextDouble();
+        List<Employee> employees = new ArrayList<>();
 
-        Employee emp = new Employee(empId, empName, empSalary);
+        while (true) {
+            inputEmpPayroll();
+            employees.add(new Employee(empId, empName, empSalary));
 
-        Path path = Paths.get("C:\\Users\\mahaj\\Desktop\\229\\Day27-IO-JavaProgram\\src\\com\\IOprogram\\Payroll_Service");
+            System.out.print("Do you wan't to add one more Employee entry[Yes/No]: ");
+            option = scanner.next();
+
+            if (option.equalsIgnoreCase("nO"))
+                break;
+        }
+
+        Path path = Paths.get("C:\\\\Users\\\\mahaj\\\\Desktop\\\\229\\\\Day27-IO-JavaProgram\\\\src\\\\com\\\\IOprogram\\\\Payroll_Service");
+
+        List<String> empsList = employees
+                .stream().map(emp -> emp.toString())
+                .collect(Collectors.toList());
 
         try {
-            Files.writeString(path, emp.toString(), StandardOpenOption.CREATE);
-            System.out.println("Employee information saved successfully to file!\n" + path);
+            if (! Files.exists(path))
+                Files.createFile(path);
+
+            Files.write(path, empsList, StandardOpenOption.APPEND);
+            System.out.println(employees.size() + " Employee information saved successfully to file!\n" + path);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
